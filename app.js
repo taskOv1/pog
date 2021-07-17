adbb90dc3699cad3cf947390b5ca26d6a47c59c0
taskOv1 <stass1379@gmail.com>
2021-07-17 13:11:42 +0300

    add file for testing on index page

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const app = express();
 const router_en = require('./routers/router_en');
 const router_ru = require('./routers/router_ru');
 
-// app.use(express.static('./public'));
+app.use(express.static('./public'));
+app.use('/', express.static('./public/en'));
 app.use('/en/', express.static('./public'));
 app.use('/ru', express.static('./public'));
 app.use('/en',router_en);
 app.use('/ru', router_ru)
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found');
+})
 // app.use('/', router);
 // app.get('/', (req, res) => {
 //   res.send('hello');
