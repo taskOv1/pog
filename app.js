8cb69d0b48bf33b4d6d888a3912581c194bfca53
taskOv1 <stass1379@gmail.com>
2021-07-21 15:42:47 +0300

    updating for favicon, and changing in slider

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const fs = require('fs');
+const router = express.Router();
 const app = express();
 
 const router_en = require('./routers/router_en');
@@ -7,13 +8,22 @@ const router_ru = require('./routers/router_ru');
 
 app.use(express.static('./public'));
 app.use('/', express.static('./public/en'));
+
 app.use('/en/', express.static('./public'));
 app.use('/ru', express.static('./public'));
 app.use('/en',router_en);
-app.use('/ru', router_ru)
+app.use('/ru', router_ru);
 app.use('/game', (req, res) => {
   const file = `${__dirname}/public/en/PawnOfTheGods.rar`;
   res.download(file);
+})
+
+app.use('/', (req, res) => {
+  if(req.url.match("/requestFavicon") ){
+    var img = fs.readFileSync('public/favicon.ico');
+    res.writeHead(200, {'Content-Type': 'image/x-icon' });
+    res.end(img, 'binary');
+}
 })
 
 app.all('*', (req, res) => {
