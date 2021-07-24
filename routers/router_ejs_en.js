b62116f3921283cc864b5987d887a5c853341005
taskOv1 <stass1379@gmail.com>
2021-07-24 12:06:01 +0300

    code to stay at the same page but in another language

--- routers/router_ejs_en.js
+++ routers/router_ejs_en.js
@@ -3,23 +3,31 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  console.log(req.url);
   res.render('./en/index', {
     title: 'Home',
+    currentUrlForLanguageSwitcher: `/ru${req.url}`,
   });
 });
 router.get('/about', (req, res) => {
+  console.log(req.url);
   res.render('./en/about', {
     title: 'About',
+    currentUrlForLanguageSwitcher: `/ru${req.url}`,
   });
 });
 router.get('/demo', (req, res) => {
+  console.log(req.url);
   res.render('./en/demo', {
     title: 'Demo',
+    currentUrlForLanguageSwitcher: `/ru${req.url}`,
   });
 });
 router.get('/gallery', (req, res) => {
+  console.log(req.url);
   res.render('./en/gallery', {
     title: 'Gallery',
+    currentUrlForLanguageSwitcher: `/ru${req.url}`,
   });
 });
 
--- routers/router_ejs_ru.js
+++ routers/router_ejs_ru.js
@@ -5,21 +5,25 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render('./ru/index', {
     title: 'Главная',
+    currentUrlForLanguageSwitcher: `/en${req.url}`,
   });
 });
 router.get('/about', (req, res) => {
   res.render('./ru/about', {
     title: 'Об авторе',
+    currentUrlForLanguageSwitcher: `/en${req.url}`,
   });
 });
 router.get('/demo', (req, res) => {
   res.render('./ru/demo', {
     title: 'Демо',
+    currentUrlForLanguageSwitcher: `/en${req.url}`,
   });
 });
 router.get('/gallery', (req, res) => {
   res.render('./ru/gallery', {
     title: 'Галлерея',
+    currentUrlForLanguageSwitcher: `/en${req.url}`,
   });
 });
 
